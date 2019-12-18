b458cc6f0b92543b03c53b626df24436fde3a37f
IonMarcoci <59025317+IonMarcoci@users.noreply.github.com>
2019-12-18 18:47:10 +0200

    Ion Marcoci

---
+++ Brad.java
@@ -0,0 +1,28 @@
+
+public class Brad {
+
+	public static void main(String[] args) {
+		 char k = '*';
+		 char l = '#';
+         char f = '^' ;
+System.out.printf("%8c%n" , k);
+System.out.printf("%7c%1c%1c%n" , k , f , k);
+System.out.printf("%6c%1c%1c%1c%1c%n" , k ,f ,k , f, k);
+System.out.printf("%5c%1c%1c%1c%1c%1c%1c%n" , k , f , k ,f , k , f , k);
+System.out.printf("%4c%1c%1c%1c%1c%1c%1c%1c%1c%n" , k , f , k , f , k , f , k , f , k , f , k);
+System.out.printf("%3c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%n" , k , f , k , f , k , f ,k , f , k , f , k , f , k);
+System.out.printf("%2c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%n" ,k , f , k , f , k , f ,k , f , k , f , k , f , k, f , k , f , k);
+System.out.printf("%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%1c%n" , k , f , k , f , k , f ,k , f , k , f , k , f , k, f , k , f , k , f , k);
+System.out.printf("%7c%1c%1c%n" , l , l , l);
+System.out.println("Craciun Fericit");
+
+
+
+
+
+
+
+
+	}
+
+}
